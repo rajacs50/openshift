655abbbf353dbdd621f7b9aa0416c890dafd54bb
rajacs50 <56812923+rajacs50@users.noreply.github.com>
2020-05-10 11:04:36 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,362 @@
+import os
+
+import sqlite3
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+from helpers import apology, login_required, lookup, usd, portfolio, tot, transaction
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure sqlite3
+conn = sqlite3.connect('finance.db', check_same_thread=False)
+conn.row_factory = sqlite3.Row
+db = conn.cursor()
+
+
+# Make sure API key is set
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+    # Query the DB for symbol, company name, num of share, for the given user
+    uid = dict(cuid=session['user_id'])
+    hs = db.execute("SELECT b.ticker, sh.company, IFNULL((b.num_shares-s.num_shares),b.num_shares) AS num_shares FROM buy b JOIN shares sh ON b.ticker=sh.ticker LEFT JOIN sell s ON b.cuid=s.cuid AND b.ticker=s.ticker where b.cuid=:cuid GROUP BY b.ticker, sh.company ORDER BY sh.company", uid)
+    # the query returns row objects, convert them to dict objects using list comprehension
+    hold_share = [dict(row) for row in hs.fetchall()]
+    # Call the helper func on the list returned by the above function
+    folio = portfolio(hold_share)
+    # Call the helper func for the total value based on current share val
+    total = float(tot())
+    # Query for getting the current user cash bal and then change the dict in the list to float
+    cd = db.execute("SELECT cash FROM users WHERE id=:cuid", uid)
+    cash_dict = [dict(row) for row in cd.fetchall()]
+    avail_bal = float(cash_dict[0]['cash'])
+    # Grand total is comprised of the avail bal, and the current holding share val
+    grand_total = total + avail_bal
+
+    return render_template("index.html", folio=folio, grand_total=usd(grand_total), avail_bal=usd(avail_bal))
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+    # User reached route via GET display the buy page
+    if request.method == "GET":
+        return render_template("buy.html")
+    # Otherwise reached through POST (as by submitting a form via POST)
+    else:
+        # check if the ticker is valid and return apology if not
+        if not request.form.get("symbol"):
+            return apology("Missing Symbol", 400)
+
+        # Ensure a positive integer for number of shares was submitted
+        elif not request.form.get("shares"):
+            return apology("Missing Shares", 400)
+
+        else:
+            # Pass the user inputted str to lookup helper func
+            ticker = lookup(request.form.get("symbol"))
+            # If  the response is empty, return apology
+            if ticker is None:
+                return apology("invalid symbol", 400)
+            else:
+                # if valid check how much cash the user has and how much the user selected shares cost
+                # query user's available balance
+                uid = dict(cuid=session['user_id'])
+                cd = db.execute("SELECT cash FROM users WHERE id=:cuid", uid)
+                cash_list = [d['cash'] for d in cd]
+                avail_bal = int(cash_list[0])
+                # the return of price from the lookup above will be in dict, separate the price
+                sel_share_val = ticker["price"]
+                # get the number of shares from user input
+                num_shares = int(request.form.get("shares"))
+                # if user hasn't enough money to buy complete the transaction return apology
+                if sel_share_val * num_shares > avail_bal:
+                    return apology("You don't have enough balance!", 400)
+                # Otherwise add the share to the user acc and deduct the money
+                else:
+                    # insert the purchase details in the db
+                    print(uid, type(uid['cuid']))
+                    sym = dict(ticker=ticker['symbol'])
+                    pa = dict(price_atm=sel_share_val)
+                    td = dict(trans_detail='by')
+                    ns = dict(num_shares=num_shares)
+                    db.execute("INSERT INTO buy ('cuid', 'ticker', 'price_atm', 'trans_detail', 'num_shares') VALUES (:cuid, :ticker, :price_atm, :trans_detail, :num_shares)", (uid["cuid"], sym["ticker"], pa["price_atm"], td["trans_detail"], ns["num_shares"]))
+                    # calculate the remaining bal for user after deducting his purchase
+                    bal_updt = avail_bal - (sel_share_val * num_shares)
+                    # update user acc with the new bal
+                    bu = dict(cash=bal_updt)
+                    db.execute("UPDATE users SET cash = :cash WHERE id = :cuid", (bu["cash"], uid["cuid"]))
+                    conn.commit()
+                    # display confirmation message and then return to portfolio
+                    flash("Transaction(s) completed Successfully!")
+                    # Redirect user to home page
+                    return redirect("/")
+
+@app.route("/check", methods=["GET"])
+def check():
+    """Return true if username available, else false, in JSON format"""
+    return jsonify("TODO")
+
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    # Query the DB for symbol, num of share, curr_price, transacted time for the given user
+    uid = dict(cuid=session['user_id'])
+    st = db.execute("SELECT ticker, CASE WHEN trans_detail = 'by' THEN num_shares ELSE '-' || num_shares END AS num_shares, price_atm, purchase_time FROM trans where cuid=:cuid ORDER BY purchase_time", uid)
+    share_trans = [dict(row) for row in st.fetchall()]
+    history = transaction(share_trans)
+    return render_template("history.html", history=history)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        # u = request.form.get("username")
+        username = dict(username=request.form.get("username"))
+        db.execute("SELECT * FROM users WHERE username = :username", username)
+        rows = [dict(row) for row in db.fetchall()]
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            print(check_password_hash(rows[0]["hash"], request.form.get("password")))
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/password", methods=["GET", "POST"])
+def password():
+    """Lets the user change their password"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not (request.form.get("password") and request.form.get("confirmation")):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        username = dict(username=request.form.get("username"))
+        db.execute("SELECT * FROM users WHERE username = :username", username)
+        check_username = [dict(row) for row in db.fetchall()]
+        
+        # Check if username exists in the DB
+        if len(check_username) == 0:
+            flash("Sorry, username does not exist!")
+            return render_template("password.html")
+        
+        else:
+            # Hash the password and update the table
+            h_pass = generate_password_hash(request.form.get("password"))
+            hp = dict(hashed_pass=h_pass)
+            db.execute("UPDATE users SET 'hash'=:hashed_pass", hp)
+            conn.commit()
+            return redirect("/")
+
+    else:
+        return render_template("password.html")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    # User reached route via GET
+    if request.method == "GET":
+        return render_template("quote.html")
+    
+    # User reached route via POST
+    else:
+        # Pass the user inputted str to lookup helper func
+        ticker = lookup(request.form.get("symbol"))
+        # If  the response is empty, return apology
+        if ticker is None:
+            return apology("invalid symbol", 400)
+        # Otherwise, return the dict from lookup to quoted
+        else:
+            return render_template("quoted.html", ticker=ticker)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not (request.form.get("password") and request.form.get("confirmation")):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        username = dict(username=request.form.get("username"))
+        db.execute("SELECT * FROM users WHERE username = :username", username)
+        check_username = [dict(row) for row in db.fetchall()]
+        
+        # Ensure username does not exist in the DB
+        if len(check_username) > 0:
+            flash("Sorry, username is already taken!")
+            return render_template("register.html")
+        
+        else:
+            # Add username, hash the password and add it to the DB
+            h_pass0 = generate_password_hash(request.form.get("password"))
+            hp = dict(h_pass=h_pass0)
+            db.execute("INSERT INTO users ('username', 'hash') VALUES (:username, :hashed_pass)", (username["username"], hp["h_pass"]))
+            flash("Registration Successfull!")
+            session["user_id"] = db.lastrowid
+            return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+    # Query the user inputted str to check if user has that in holding
+    uid = dict(cuid=session['user_id'])
+    db.execute("SELECT b.ticker, IFNULL((b.num_shares-s.num_shares),b.num_shares) AS num_shares FROM buy b LEFT JOIN sell s ON b.cuid=s.cuid AND b.ticker=s.ticker where b.cuid=:cuid GROUP BY b.ticker ORDER BY b.ticker", uid)
+    own_share = db.fetchall()
+    # SET comprehension to get the unique ticker names
+    share_avail = {l['ticker'] for l in own_share}
+    # User reached route via GET display the sell page
+    if request.method == "GET":
+        return render_template("sell.html", share_avail=share_avail)
+    # Otherwise reached through POST (as by submitting a form via POST)
+    else:
+        # check if the ticker is valid and return apology if not
+        if not request.form.get("symbol"):
+            return apology("Missing Symbol", 400)
+
+        # Ensure a positive integer for number of shares was submitted
+        elif not request.form.get("shares"):
+            return apology("Missing Shares", 400)
+        # Ensure that the ticker name is available in the shares owned by the user
+        elif request.form.get("symbol") not in share_avail:
+            return apology("You don't own this share!", 400)
+
+        else:
+            for share in own_share:
+                if share['ticker'] == request.form.get("symbol"):
+                    if share['num_shares'] < int(request.form.get("shares")):
+                        return apology("Too many Shares", 400)
+                    else:
+                        # since the return of price will be in dict, separate the price
+                        price = lookup(request.form.get("symbol"))
+                        sel_share_val = price['price']
+                        # get the number of shares from user input
+                        num_shares = int(request.form.get("shares"))
+                        # insert the sold details in the db
+                        sym = dict(ticker=request.form.get("symbol"))
+                        pa = dict(price_atm=sel_share_val)
+                        td = dict(trans_detail='sl')
+                        ns = dict(num_shares=num_shares)
+                        db.execute("INSERT INTO sell ('cuid', 'ticker', 'price_atm', 'trans_detail', 'num_shares') VALUES (:cuid, :ticker, :price_atm, :trans_detail, :num_shares)", (uid["cuid"], sym["ticker"], pa["price_atm"], td["trans_detail"], ns["num_shares"]))
+                        # Query for getting the current user cash bal and then change the dict in the list to float
+                        cd = db.execute("SELECT cash FROM users WHERE id=:cuid", uid)
+                        cash_dict = [dict(row) for row in cd.fetchall()]
+                        avail_bal = float(cash_dict[0]['cash'])
+                        # calculate the remaining bal for user after deducting his purchase
+                        bal_updt = avail_bal + (sel_share_val * num_shares)
+                        # update user acc with the new bal
+                        bu = dict(cash=bal_updt)
+                        db.execute("UPDATE users SET cash = :cash WHERE id = :cuid", (bu["cash"], uid["cuid"]))
+                        conn.commit()
+                        # display confirmation message and then return to portfolio
+                        flash("Transaction(s) completed Successfully!")
+                        # Redirect user to home page
+                        return redirect("/")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
---
+++ helpers.py
@@ -0,0 +1,124 @@
+import os
+import requests
+import urllib.parse
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+def lookup(symbol):
+    """Look up quote for symbol."""
+
+    # Contact API
+    try:
+        # api_key = os.environ.get("API_KEY")
+        api_key = "pk_c198ae541c8d47cabe6bd126e427c2d4"
+        response = requests.get(f"https://cloud-sse.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}")
+        response.raise_for_status()
+    except requests.RequestException:
+        return None
+
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"]
+        }
+    except (KeyError, TypeError, ValueError):
+        return None
+
+
+def usd(value):
+    """Format value as USD."""
+    return f"${value:,.2f}"
+
+
+def portfolio(hold_share):
+    """
+    Expects a list of dict as the input, iterates through each dict elem from the list
+    outputs another list with the symbol, num of shares, curr price (using lookup helper func)
+    total holding share val
+    """
+    share_detail = []
+    global total
+    total = 0
+    for share in hold_share:
+        stock = {}
+        stock['ticker'] = share['ticker']
+        stock['num_shares'] = share['num_shares']
+        stock['curr_price'] = lookup(stock['ticker'])
+        stock['total_holding'] = stock['num_shares'] * stock['curr_price']['price']
+        total += stock['total_holding']
+        share_detail.append(stock)
+    return share_detail
+
+
+def tot():
+    """
+    Just to return the total from the portfolio func
+    """
+    return total
+
+
+def transaction(share_trans):
+    """
+    Expects a list of dict as the input, iterates through each dict elem from the list
+    outputs another list with the symbol, num of shares, price at the time of purchase
+    transacted time.
+    """
+    share_detail = []
+    for share in share_trans:
+        stock = {}
+        stock['ticker'] = share['ticker']
+        stock['num_shares'] = share['num_shares']
+        stock['price_atm'] = share['price_atm']
+        stock['purchase_time'] = share['purchase_time']
+        share_detail.append(stock)
+    return share_detail
+
+# def transaction(*args):
+#     """
+#     Expects a list of dict as the input, iterates through each dict elem from the list
+#     outputs another list with the symbol, num of shares, price at the time of purchase
+#     transacted time.
+#     """
+#     share_detail = []
+#     for share in share_trans:
+#         stock = {}
+#         stock['ticker'] = share['ticker']
+#         stock['num_shares'] = share['num_shares']
+#         stock['price_atm'] = share['price_atm']
+#         stock['purchase_time'] = share['purchase_time']
+#         share_detail.append(stock)
+#     return share_detail
