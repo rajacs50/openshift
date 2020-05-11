357844f45c41c2da4dbfc44fc425c736ac8d5b3a
rajacs50 <56812923+rajacs50@users.noreply.github.com>
2020-05-11 11:50:32 +0530

    Started adding code changes to use SQLAlchemy, and updated req text

--- application.py
+++ application.py
@@ -1,13 +1,12 @@
 import os
 
-import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-
-
 from helpers import apology, login_required, lookup, usd, portfolio, tot, transaction
 
 # Configure application
@@ -34,11 +33,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure sqlite3
-conn = sqlite3.connect('finance.db', check_same_thread=False)
-conn.row_factory = sqlite3.Row
-db = conn.cursor()
-
+# Configure database connection
+DATABASE_URL = "postgres://drmddgon:nUUx80PXWwPZ_MH_aZWtBo9kQk4pcGbf@satao.db.elephantsql.com:5432/drmddgon"
+engine = create_engine("DATABASE_URL")
+db = scoped_session(sessionmaker(bind=engine))
 
 # Make sure API key is set
 # if not os.environ.get("API_KEY"):
@@ -50,16 +48,40 @@ db = conn.cursor()
 def index():
     """Show portfolio of stocks"""
     # Query the DB for symbol, company name, num of share, for the given user
-    uid = dict(cuid=session['user_id'])
-    hs = db.execute("SELECT b.ticker, sh.company, IFNULL((b.num_shares-s.num_shares),b.num_shares) AS num_shares FROM buy b JOIN shares sh ON b.ticker=sh.ticker LEFT JOIN sell s ON b.cuid=s.cuid AND b.ticker=s.ticker where b.cuid=:cuid GROUP BY b.ticker, sh.company ORDER BY sh.company", uid)
-    # the query returns row objects, convert them to dict objects using list comprehension
-    hold_share = [dict(row) for row in hs.fetchall()]
+    hs = db.execute("""WITH bought_shares AS (
+                            SELECT cuid, ticker, SUM(num_shares) AS num_bought_shares
+                            FROM transaction
+                            WHERE cuid=:cuid AND trans_detail='by'
+                            GROUP BY cuid, ticker
+                            ORDER BY ticker),
+                            
+                            sold_shares AS (
+                                SELECT cuid, ticker, SUM(num_shares) AS num_sold_shares
+                                FROM transaction
+                                WHERE cuid=:cuid AND trans_detail='sl'
+                                GROUP BY cuid, ticker
+                                ORDER BY ticker)
+                                
+                            SELECT t.ticker, t.company,
+                                CASE
+                                    WHEN b.num_bought_shares - s.num_sold_shares IS NULL THEN b.num_bought_shares
+                                    ELSE b.num_bought_shares - s.num_sold_shares
+                                END AS num_curr_shares
+                            FROM transaction t
+                            LEFT JOIN bought_shares b ON t.cuid=b.cuid AND t.ticker=b.ticker
+                            LEFT JOIN sold_shares s ON t.cuid=s.cuid AND t.ticker=s.ticker
+                            GROUP BY t.ticker, t.company, num_curr_shares
+                            ORDER BY t.ticker""", cuid=session['user_id']).fetchall()
+    # the query returns list of tuples, convert them to dict objects using list comprehension
+    # need to declare variables so the dict func can iterate over and create a list of dicts
+    keys = ("ticker", "company", "num_shares")
+    hold_share = [dict(zip(keys, values)) for values in hs]
     # Call the helper func on the list returned by the above function
     folio = portfolio(hold_share)
     # Call the helper func for the total value based on current share val
     total = float(tot())
     # Query for getting the current user cash bal and then change the dict in the list to float
-    cd = db.execute("SELECT cash FROM users WHERE id=:cuid", uid)
+    cd = db.execute("SELECT cash FROM users WHERE id=:cuid", cuid=session['user_id'])
     cash_dict = [dict(row) for row in cd.fetchall()]
     avail_bal = float(cash_dict[0]['cash'])
     # Grand total is comprised of the avail bal, and the current holding share val
@@ -107,7 +129,6 @@ def buy():
                 # Otherwise add the share to the user acc and deduct the money
                 else:
                     # insert the purchase details in the db
-                    print(uid, type(uid['cuid']))
                     sym = dict(ticker=ticker['symbol'])
                     pa = dict(price_atm=sel_share_val)
                     td = dict(trans_detail='by')
@@ -161,9 +182,7 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        # u = request.form.get("username")
-        username = dict(username=request.form.get("username"))
-        db.execute("SELECT * FROM users WHERE username = :username", username)
+        db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
         rows = [dict(row) for row in db.fetchall()]
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
@@ -213,12 +232,12 @@ def password():
         username = dict(username=request.form.get("username"))
         db.execute("SELECT * FROM users WHERE username = :username", username)
         check_username = [dict(row) for row in db.fetchall()]
-        
+
         # Check if username exists in the DB
         if len(check_username) == 0:
             flash("Sorry, username does not exist!")
             return render_template("password.html")
-        
+
         else:
             # Hash the password and update the table
             h_pass = generate_password_hash(request.form.get("password"))
@@ -238,7 +257,7 @@ def quote():
     # User reached route via GET
     if request.method == "GET":
         return render_template("quote.html")
-    
+
     # User reached route via POST
     else:
         # Pass the user inputted str to lookup helper func
@@ -269,20 +288,17 @@ def register():
             return apology("must provide password", 403)
 
         # Query database for username
-        username = dict(username=request.form.get("username"))
-        db.execute("SELECT * FROM users WHERE username = :username", username)
-        check_username = [dict(row) for row in db.fetchall()]
-        
+        check_username = db.execute("SELECT username FROM users WHERE username = :username", username=request.form.get("username")).fetchall()
+
         # Ensure username does not exist in the DB
         if len(check_username) > 0:
             flash("Sorry, username is already taken!")
             return render_template("register.html")
-        
+
         else:
             # Add username, hash the password and add it to the DB
-            h_pass0 = generate_password_hash(request.form.get("password"))
-            hp = dict(h_pass=h_pass0)
-            db.execute("INSERT INTO users ('username', 'hash') VALUES (:username, :hashed_pass)", (username["username"], hp["h_pass"]))
+            hashed_pass = generate_password_hash(request.form.get("password"))
+            db.execute("INSERT INTO users ('username', 'hash') VALUES (:username, :hashed_pass)", username=request.form.get("username"), hashed_pass=hashed_pass)
             flash("Registration Successfull!")
             session["user_id"] = db.lastrowid
             return redirect("/")
